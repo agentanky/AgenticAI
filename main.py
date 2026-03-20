c0857f302159c10f8ec22449d2032f33a6a07663
Ankur Tandan <tandan@gmail.com>
2026-03-19 17:26:54 -0700

    added token metadata

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ from google import genai
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model = "gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+
+
 
 
 def main():
+
+    response = client.models.generate_content(
+    model = "gemini-2.5-flash", contents="tell me about earth in one paragraph"
+)
+    if response.usage_metadata is None:
+        raise RuntimeError("no token information available")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
