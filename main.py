317321358a11b474a6d0f817f5f84e14b935e350
Ankur Tandan <tandan@gmail.com>
2026-03-28 17:06:39 -0700

    added system prompts and temperature

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 
 from google import genai
 from google.genai import types
+from prompts import system_prompt
 
 
 def main():
@@ -27,7 +28,10 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-    model = "gemini-2.5-flash", contents=messages
+    model = "gemini-2.5-flash", contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt, temperature = 0),
+    
+
 )
     if response.usage_metadata is None:
         raise RuntimeError("no token information available")
