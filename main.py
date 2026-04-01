7978c4dae96dbb05ded31ef6568eb09c543a0c89
Ankur Tandan <tandan@gmail.com>
2026-04-01 16:16:32 -0700

    added agent loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from dotenv import load_dotenv
+import sys
 
 from google import genai
 from google.genai import types
@@ -30,33 +31,43 @@ def main():
         print(f"User prompt: {args.user_prompt}")
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    for _ in range(20):
+            
+            response = client.models.generate_content(
+            model = "gemini-2.5-flash", contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt, temperature = 0),
+            
+        )
+            function_responses = []
+            if response.candidates:
+                for item in response.candidates:
+                    messages.append(item.content)
 
-    response = client.models.generate_content(
-    model = "gemini-2.5-flash", contents=messages,
-    config=types.GenerateContentConfig(tools=[available_functions],system_instruction=system_prompt, temperature = 0),
-    
-
-)
-    if response.usage_metadata is None:
-        raise RuntimeError("no token information available")
-    if args.verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    if response.function_calls is not None:
-        for item in response.function_calls:
-            function_call_result = call_function(item, args.verbose)
-            if function_call_result.parts is None:
-                raise Exception("parts is none")
-            if function_call_result.parts[0].function_response is None:
-                raise Exception("parts response is empty")
-            if function_call_result.parts[0].function_response.response is None:
-                raise Exception("response empty")
+            if response.usage_metadata is None:
+                raise RuntimeError("no token information available")
             if args.verbose:
-                print(f"-> {function_call_result.parts[0].function_response.response}")
-             
-
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            if response.function_calls is not None:
+                for item in response.function_calls:
+                    function_call_result = call_function(item, args.verbose)
+                    if function_call_result.parts is None:
+                        raise Exception("parts is none")
+                    if function_call_result.parts[0].function_response is None:
+                        raise Exception("parts response is empty")
+                    if function_call_result.parts[0].function_response.response is None:
+                        raise Exception("response empty")
+                    if args.verbose:
+                        print(f"-> {function_call_result.parts[0].function_response.response}")
+                    function_responses.append(function_call_result.parts[0])
+                messages.append(types.Content(role="user", parts=function_responses))
+            else:
+                print(response.text)
+                break
     else:
-        print(response.text)
+        print(f"model has not produced final response")
+        raise SystemExit(1)  
+            
 
 
 if __name__ == "__main__":
